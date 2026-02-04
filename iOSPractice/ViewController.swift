91e241b677a93c6572501da6076a50f38349e3a7
Hari.Dev <hariravi.dev.1004@gmail.com>
2026-02-04 15:23:52 +0530

    initial commit

---
+++ iOSPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSPractice
+//
+//  Created by Hariharan Ravichandran on 2/4/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
